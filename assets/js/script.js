89ef4d4b0a603940aa8fb75e67c148d04206d12d
EricMalpass <ericmalpass@gmail.com>
2021-03-20 10:18:30 -0700

    add pics

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,25 +9,38 @@ var buttonClickHandler = function (event) {
         return;
     }
   var alcoholPick = event.target.getAttribute('data-alcohol');
-  console.log(alcoholPick)
-    if (alcoholPick) {
-        getAlcohol(alcoholPick);
+  //var pick2 = event.target.getAttribute('data-mixers')
+  console.log(alcoholPick) //pick2 out
+    if (alcoholPick) {//pick2 out
+        getAlcohol(alcoholPick); //pick2 out
      } 
   };
-  var getAlcohol = function (alcoholPick) {
-    console.log(alcoholPick);
-<<<<<<< HEAD
-    var apiUrl = "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i="+ alcoholPick +'';
-=======
-    var apiUrl = "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=" + alcoholPick+'' ;
->>>>>>> c74f20e53dab82f8914759ca6af44a3c4a2a4d33
+  var getAlcohol = function (alcoholPick) { //pick2
+    console.log(alcoholPick); //pick2
+
+    var apiUrl = "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=" + alcoholPick; //pick2 out
     fetch(apiUrl)
       .then(function (response) {
         if (response.ok) {
           response.json().then(function (data) {
             console.log(data);
-            drinkDisplay(data, alcoholPick);
-          
+            //drinkDisplay(data, alcoholPick);//pick2 out
+
+            var alcoholDisplay = document.getElementById("imgDisplay");
+            alcoholDisplay.setAttribute("src", data.drinks[0].strDrinkThumb)
+            console.log(alcoholDisplay);
+
+             var titleDisplay = document.getElementById("titleDisplay");
+             titleDisplay.innerHTML = data.drinks[0].strDrink;
+             console.log(titleDisplay);
+
+             var alcoholDisplay1 = document.getElementById("imgDisplay1");
+            alcoholDisplay1.setAttribute("src", data.drinks[1].strDrinkThumb)
+            console.log(alcoholDisplay1);
+
+             var titleDisplay1 = document.getElementById("titleDisplay1");
+             titleDisplay1.innerHTML = data.drinks[1].strDrink;
+             console.log(titleDisplay1);
           });
         } else {
           alert('Error: ' + response.statusText);
@@ -37,13 +50,16 @@ var buttonClickHandler = function (event) {
         alert('Unable to connect to bartender');
       });
   };
+  //var drinkDisplay {
+
+  //}
+
+   //var alcoholDisplay = document.createElement("imgDisplay");
+         //alcoholDisplay.setAttribute ("src", `data.drinks[3].strDrinkThumb.jpg`)
+          //console.log(alcoholDisplay.src);
+          //console.log(alcoholDisplay);
   
-<<<<<<< HEAD
-
 
   alcoholButtonEl.addEventListener('click', buttonClickHandler);
-=======
-  alcoholButtonEl.addEventListener('click', buttonClickHandler);
+  //mixerButtonEl.addEventListener('click', buttonClickHandler2);
 
-
->>>>>>> c74f20e53dab82f8914759ca6af44a3c4a2a4d33
