7f5069da60c0ea0cb51f787cd69cdd2dcf77af34
Sam Oberg <samueloberg@hotmail.com>
2021-03-21 13:16:00 -0700

    nothing

--- assets/js/samsjs.js
+++ assets/js/samsjs.js
@@ -1,12 +1,12 @@
-var modalBtn = document.querySelectorAll('#modalBtn');
+var modalBtn = document.querySelectorAll('.modalbtn');
 var modal = document.querySelector('.modal');
 var mainPage = document.querySelector('main');
 var asidePage = document.querySelector('.ingredients');
 var header = document.querySelector('.title')
-console.log(modalBtn)
+console.log(titleDisplay1.value)
 
 for (var i = 0; i < modalBtn.length; i++){
-    console.log(i)
+    
     modalBtn[i].addEventListener('click', function() {
         modal.style.display = 'block';
         mainPage.style.display = 'none';
@@ -22,3 +22,23 @@ document.querySelector('#closeBtn').addEventListener('click', function () {
     asidePage.style.display = 'block';
     header.style.display = 'block';
 });
+
+// function testValue() {
+//     if (titleDisplay = 'undefined') {
+//         console.log('test')
+
+//     } else if(titleDisplay1 = 'undefined') {
+
+//     } else if(titleDisplay2 = 'undefined') {
+
+//     } else if(titleDisplay3 = 'undefined') {
+
+//     } else if(titleDisplay4 = 'undefined') {
+        
+//     } else {
+//         console.log('no value selected')
+//         document.querySelector('.grid').style.display = 'none';
+//         document.querySelector('.errorMessage').style.display = 'block'
+//     }
+// }
+// testValue();
